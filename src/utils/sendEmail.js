7447e0084d3d52630ca49e0c435b9cac7171e004
Mohamed El omary <elomarymohamed72@gmail.com>
2025-10-05 15:48:45 +0100

    get back email update

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 export default async function sendEmail({ to, template, subject }) {
   const transporter = nodemailer.createTransport({
     host: "smtp.zoho.com",
-    port: 587, // Use 587 if you prefer TLS
-    secure: false, // Use false if you're using 587
+    port: 465, // Use 587 if you prefer TLS
+    secure: true, // Use false if you're using 587
     auth: {
       user: process.env.EMAIL,
       pass: process.env.EMAIL_PASSWORD,
