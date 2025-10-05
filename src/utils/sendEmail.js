9ac184404c148a221b9dd26e19208958ed69bac0
Mohamed El omary <elomarymohamed72@gmail.com>
2025-10-05 15:42:19 +0100

    update send mail

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,25 +1,20 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
-export default async function  sendEmail({
+export default async function sendEmail({ to, template, subject }) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.zoho.com",
+    port: 465, // Use 587 if you prefer TLS
+    // secure: false, // Use false if you're using 587
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  await transporter.sendMail({
+    from: process.env.EMAIL,
     to,
-    template,
-    subject
-})
-{
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.zoho.com',
-        port: 465, // Use 587 if you prefer TLS
-        secure: true, // Use false if you're using 587
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.EMAIL_PASSWORD,
-        },
-    })
-
-    await transporter.sendMail({
-        from: process.env.EMAIL,
-        to,
-        html: template,
-        subject
-    })
+    html: template,
+    subject,
+  });
 }
